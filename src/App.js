9a7a4500a4e86f06fb8305d1cfdd295072917a58
akietit1001 <ndakiet1001@gmail.com>
2022-11-23 10:14:13 +0700

    Create Message, Setting and Notificaion page

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import {
   BrowserRouter as Router,
   Routes,
   Route,
-  Navigate,
-  useLocation,
 } from "react-router-dom";
 import { ROUTE } from './utils/constants'
 import Login from './pages/Login/Login';
 import Register from './pages/Register/Register';
 import ForgetPassword from './pages/ForgetPassword/ForgetPassword';
+import Home from './pages/Home/Home';
+import Notification from './pages/Notification/Notification';
+import Profile from './pages/Profile/Profile';
+import Messages from './pages/Messages/Messages';
+import Setting from './pages/Setting/Setting';
 
 function App() {
   return (
@@ -27,6 +30,26 @@ function App() {
             <Route 
               path={ROUTE.FORGET_PASSWORD.URL}
               element={<ForgetPassword />}
+            />
+            <Route 
+              path={ROUTE.HOME.URL}
+              element={<Home />}
+            />
+            <Route 
+              path={ROUTE.MESSAGES.URL}
+              element={<Messages />}
+            />
+            <Route 
+              path={ROUTE.NOTIFY.URL}
+              element={<Notification />}
+            />
+            <Route 
+              path={ROUTE.PROFILE.URL}
+              element={<Profile />}
+            />
+            <Route 
+              path={ROUTE.SETTING.URL}
+              element={<Setting />}
             />
           </Routes>
         </div>
--- src/layouts/Sidebar/Sidebar.js
+++ src/layouts/Sidebar/Sidebar.js
@@ -1,10 +1,22 @@
 import styles from './Sidebar.module.scss';
-import classnames from 'classnames/bind';
+import classNames from 'classnames/bind';
+import Menu, {MenuItem} from './Menu';
+import { ROUTE } from '../../utils/constants';
+import images from './../../assets/index'
 
-const cx = classnames.bind(styles);
+const cx = classNames.bind(styles);
 
 function Sidebar() {
-    return <div className={cx('wrapper')}>Hello World</div>
+    return <aside className={cx('wrapper')}>
+    <img src= {images.logo} alt='logo'/>
+    <Menu>
+        <MenuItem title={"Home"} to={ROUTE.HOME.URL}/>
+        <MenuItem title={"Messages"} to={ROUTE.MESSAGES.URL}/>
+        <MenuItem title={"Notifications"} to={ROUTE.NOTIFY.URL} />
+        <MenuItem title={"Profile"} to={ROUTE.PROFILE.URL} />
+        <MenuItem title={"Setting"} to={ROUTE.SETTING.URL} />
+    </Menu>
+</aside>
 }
 
 export default Sidebar;
