4a38fe07050be9a02a50184ad63dad160348ea98
akietit1001 <ndakiet1001@gmail.com>
2023-01-29 10:16:46 +0700

    edit register page

--- src/pages/Register/Register.js
+++ src/pages/Register/Register.js
@@ -10,11 +10,16 @@ import InputAdornment from '@mui/material/InputAdornment';
 import IconButton from '@mui/material/IconButton';
 import VisibilityIcon from '@mui/icons-material/Visibility';
 import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
-import { DatePicker, Radio, Checkbox } from 'antd';
+import Select from '@mui/material/Select';
+import MenuItem from '@mui/material/MenuItem';
+import Radio from '@mui/material/Radio';
+import RadioGroup from '@mui/material/RadioGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormLabel from '@mui/material/FormLabel';
+import { DatePicker, Checkbox } from 'antd';
 import Button from '@mui/material/Button';
 import images from '../../assets';
 import userApi from '../../apis/userApi';
-const dayjs = require('dayjs')
 
 
 const cx = classnames.bind(styles);
@@ -31,7 +36,7 @@ function Register() {
     const [occupation, setOccupation] = useState('');
     const [birthDay, setBirthDay] = useState('');
     const [gender, setGender] = useState('');
-    const [role, setRole] = useState(['Admin']);
+    const [role, setRole] = useState([]);
     const [password, setPassword] = useState('');
     const [conformPassword, setConformPassword] = useState('');
     const [showPassword1, setShowPassword1] = useState(false);
@@ -157,21 +162,29 @@ function Register() {
                     sx={{fontSize: '16px'}}
                 />
             </FormControl>
-            
-            <FormControl sx={{ m: 1, width: '35ch'}} variant="filled">
-                <InputLabel htmlFor="filled-adornment-occupation" sx={{fontSize: '14px'}}>Occupation</InputLabel>
-                <FilledInput
-                    id="filled-adornment-occupation"
-                    required = {true}
-                    onChange={(e)=>{
-                        setOccupation(e.target.value);
-                    }}
-                    sx={{fontSize: '16px'}}
-                />
+
+            <FormControl variant="filled" sx={{ m: 1, width: '35ch'}}>
+                <InputLabel id="demo-simple-select-filled-label" sx={{fontSize: '14px'}}>Occupation</InputLabel>
+                <Select
+                labelId="demo-simple-select-filled-label"
+                id="demo-simple-select-filled"
+                value={occupation}
+                onChange={(e)=>{
+                    setOccupation(e.target.value);
+                }}
+                sx={{fontSize: '16px'}}
+                >
+                <MenuItem value="">
+                    <em>None</em>
+                </MenuItem>
+                <MenuItem value={'Student'} sx={{fontSize: '16px'}}>Student</MenuItem>
+                <MenuItem value={'Teacher'} sx={{fontSize: '16px'}}>Teacher</MenuItem>
+                <MenuItem value={'Bussiness'} sx={{fontSize: '16px'}}>Bussiness</MenuItem>
+                </Select>
             </FormControl>
 
             <DatePicker 
-                style={{width: '95%', margin: '10px 0'}}
+                style={{width: '63%', margin: '10px 0'}}
                 placeholder="Select birthday"
                 size='large'
                 onChange={(date, dateString)=>{
@@ -179,20 +192,26 @@ function Register() {
                 }}
             />
 
-            <Radio.Group onChange={(e)=>{
-                setGender(e.target.value)
-            }} value={gender}
-            size='large'
-            style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-between', width: '40%'}}>
-                    <Radio value={"male"}>Male</Radio>
-                    <Radio value={"female"}>Female</Radio>
-            </Radio.Group>
+            <FormControl style={{width: '95%', justifyContent: 'center', alignItems: 'center', flexDirection: 'row', fontSize: '16px'}}>
+            <FormLabel id="demo-row-radio-buttons-group-label" style={{marginRight: '10px', fontSize: '16px'}}>Gender: </FormLabel>
+            <RadioGroup
+                aria-labelledby="demo-row-radio-buttons-group-label"
+                name="radio-buttons-group"
+                style={{flexDirection: 'row'}}
+                onChange={(e)=>{
+                    setGender(e.target.value)
+                }}
+            >
+                <FormControlLabel value="female" control={<Radio />} label="Female" sx={{fontSize: '16px'}}/>
+                <FormControlLabel value="male" control={<Radio />} label="Male" sx={{fontSize: '16px'}}/>
+                <FormControlLabel value="other" control={<Radio />} label="Other" sx={{fontSize: '16px'}}/>
+            </RadioGroup>
+            </FormControl>
 
             <Checkbox.Group onChange={(value)=>{
                 setRole(value);
             }}
                 options={roleOptions}
-                defaultValue={['Admin']}
                 style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-between', width: '40%'}}
             >
 
